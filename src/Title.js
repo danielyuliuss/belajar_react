f74322456f839f8efd5eea674b8c9b3276026162
Daniel Yulius S <danielyuliussoetjipto@gmail.com>
2023-06-02 14:12:39 +0700

    Latihan1

---
+++ src/Title.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Title extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title : "Ini adalah title pertamaku",
+            subTitle : "Ini adalah subtitle pertamaku"
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.title}</h1>
+                <h3>{this.state.subTitle}</h3>
+            </div>
+        )
+        
+    }
+}
+
+export default Title;
